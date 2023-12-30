f7a5546ea507476371cda8debd750768f939c5d6
SairaBeg <sairabeg.dev@gmail.com>
2023-12-30 15:32:31 -0500

    Initial commit

---
+++ HRAdministrationAPI/EmployeeBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdministrationAPI
+{
+    public class EmployeeBase : IEmployee
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public virtual decimal Salary { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
