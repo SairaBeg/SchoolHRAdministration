eecd82e0998953206ed9898e8c793ce65394a241
SairaBeg <sairabeg.dev@gmail.com>
2024-01-01 15:11:27 -0500

    Generic FactoryPattern class added
    Instantiates Employee instances

---
+++ HRAdministrationAPI/FactoryPattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdministrationAPI
+{
+    public class FactoryPattern<K,T> where T: class,K,new()
+    {
+        public static K GetInstance()
+        {
+            K objK;
+            objK = new T();
+            return objK;
+
+        }
+        
+
+    }
+}
--- SchoolHRAdministration/Program.cs
+++ SchoolHRAdministration/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using HRAdministrationAPI;
 using System.Linq;
+using static SchoolHRAdministration.Program;
 
 namespace SchoolHRAdministration
 {
-    class Program
+    public class Program
     {
+        public enum EmployeeType
+        {
+            Teacher,
+            AssistantPrincipal,
+            Principal
+        }
         static void Main(string[] args)
         {
             decimal totalSalaries = 0;
@@ -16,52 +23,28 @@ namespace SchoolHRAdministration
             //        totalSalaries += employee.Salary;
             //    }
             //    Console.WriteLine(totalSalaries);
-            Console.WriteLine( employees.Sum(e=> e.Salary));
+            Console.WriteLine(employees.Sum(e => e.Salary));
         }
         public static void SeedData(List<IEmployee> employees)
         {
-            IEmployee teacher1 = new Teacher
-            {
-                Id = 1,
-                FirstName = "Bob",
-                LastName = "Fisher",
-                Salary = 40000
-            };
+            IEmployee teacher1 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 1, "Bob", "Fisher",40000);
+            IEmployee teacher2 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 2, "Jenny", "Thomas", 40000);
+            IEmployee aprincipal = EmployeeFactory.GetEmployeeInstance(EmployeeType.AssistantPrincipal, 3, "Delvin", "Brown", 70000);
+            IEmployee principal = EmployeeFactory.GetEmployeeInstance(EmployeeType.Principal, 4, "Brenda", "Mullins", 80000);
+
             employees.Add(teacher1);
 
-            IEmployee teacher2 = new Teacher
-            {
-                Id = 2,
-                FirstName = "Jenny",
-                LastName = "Thomas",
-                Salary = 40000
-            };
             employees.Add(teacher2);
 
-            IEmployee aprincipal = new AssistantPrincipal
-            {
-                Id = 3,
-                FirstName = "Delvin",
-                LastName = "Brown",
-                Salary = 70000
-            };
             employees.Add(aprincipal);
 
-
-            IEmployee principal = new Principal
-            {
-                Id = 4,
-                FirstName = "Brenda",
-                LastName = "Mullins",
-                Salary = 80000
-            };
             employees.Add(principal);
         }
     }
 
-public class Teacher : EmployeeBase
+    public class Teacher : EmployeeBase
     {
-        public override decimal Salary { get => base.Salary+  (base.Salary *0.02m); }
+        public override decimal Salary { get => base.Salary + (base.Salary * 0.02m); }
     }
 
     public class AssistantPrincipal : EmployeeBase
@@ -71,5 +54,41 @@ public class Teacher : EmployeeBase
     public class Principal : EmployeeBase
     {
         public override decimal Salary { get => base.Salary + (base.Salary * 0.05m); }
+    }
+
+    public static class EmployeeFactory
+    {
+        public static IEmployee GetEmployeeInstance(EmployeeType employeeType, int id, string firstName, string lastName, decimal salary)
+        {
+            IEmployee employee = null;
+
+            switch (employeeType)
+            {
+                case EmployeeType.Teacher:
+                    employee = FactoryPattern<IEmployee, Teacher>.GetInstance();
+                    break;
+
+                case EmployeeType.AssistantPrincipal:
+                    employee = FactoryPattern<IEmployee, AssistantPrincipal>.GetInstance();
+                    break;
+
+                case EmployeeType.Principal:
+                    employee = FactoryPattern<IEmployee, Principal>.GetInstance();
+                    break;
+
+            }
+            if (employee != null)
+            {
+                employee.Id = id;
+                employee.FirstName = firstName;
+                employee.LastName = lastName;
+                employee.Salary = salary;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+            return employee;
+        }
     }
 }
